1d60ec92ac6b696079d516625fa6133ebcec1a50
tic-pima <alverromero11@gmail.com>
2024-04-11 09:07:26 -0400

    cambios en cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         /*$middleware->validateCsrfTokens(except: [
             env('APP_URL')
         ]);*/
-        $middleware->append(Cors::class);
+        $middleware->statefulApi();
+        //$middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
