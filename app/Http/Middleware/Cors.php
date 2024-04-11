63cab65b6de802ebc1f04a88ffa5398eb7ce2537
tic-pima <alverromero11@gmail.com>
2024-04-11 16:46:38 -0400

    cambios de cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', env('CORS_DOMAIN'));
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,10 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        /*$middleware->validateCsrfTokens(except: [
-            env('APP_URL')
-        ]);*/
-        //$middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            env('APP_URL') . '/researchjspost'
+        ]);
         $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
